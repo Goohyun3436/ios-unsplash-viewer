f34d8b81d09cb21bfd28acd82c1137bce0289019
Goohyun3436 <3436hyun@gmail.com>
2025-01-23 08:54:37 +0900

    test: BaseView 구조 개선중

--- ios-unsplash-viewer/Base/BaseView.swift
+++ ios-unsplash-viewer/Base/BaseView.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class BaseView: UIView {
+    // 프로토콜 활용?
+    // 다른 UIImageView 커스텀 클래스를 만들지 않고 UIView 에 ImageView를 addSubView 해서, BaseView 하나만 유지..?
+    // (좀더 고민해보기)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
