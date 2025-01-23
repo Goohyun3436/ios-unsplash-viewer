a0db28f440bbcf4f8eef17086059e8341ae97329
Goohyun3436 <3436hyun@gmail.com>
2025-01-23 16:24:02 +0900

    design: 온보딩 화면 UI

---
+++ ios-unsplash-viewer/Presentation/Onboarding/ViewControllers/OnboardingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingViewController.swift
+//  ios-unsplash-viewer
+//
+//  Created by Goo on 1/23/25.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingViewController: UIViewController {
+    
+    //MARK: - UI Property
+    let button = UIButton()
+    
+    //MARK: - Override Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .darkGray
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(50)
+        }
+        button.layer.cornerRadius = 25
+        button.backgroundColor = .white
+        button.setTitleColor(.darkGray, for: .normal)
+        button.setTitle("시작하기", for: .normal)
+    }
+    
+}
